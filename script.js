a6e7258529eed5b276f333bd60d328b8c51cba06
yuheiarimoto616 <yuhei61627@icloud.com>
2023-07-10 01:34:47 +0900

    html and css styling completed

---
+++ script.js
@@ -0,0 +1,52 @@
+let mylibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    let book = new Book(title, author, pages, read);
+
+    mylibrary.push(book);
+}
+
+let addBookBtn = document.getElementById("addBookBtn");
+let modal = document.querySelector('.modal');
+let overlay = document.querySelector('.overlay');
+let submit = document.querySelector('button[type="submit"]');
+
+
+addBookBtn.addEventListener('click', displayAddBookForm);
+overlay.addEventListener('click', dismissDisplay);
+submit.addEventListener('click', addNewBook);
+
+function displayAddBookForm() {
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function dismissDisplay() {
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
+function addNewBook(e) {
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let read = document.getElementById('read').checked;
+
+    document.getElementById('title').value = "";
+    document.getElementById('author').value = "";
+    document.getElementById('pages').value = "";
+    document.getElementById('read').checked = false;
+
+    addBookToLibrary(title, author, pages, read);
+    
+    dismissDisplay();
+    e.preventDefault();
+}
+
